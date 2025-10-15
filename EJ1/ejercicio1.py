add13e9592db4b509fc7f58d8da8db5c255f8785
javier020301001 <rjsaransig@uce.edu,ec>
2025-10-15 16:11:42 -0500

    Primer ejercicio python

---
+++ EJ1/ejercicio1.py
@@ -0,0 +1,21 @@
+#Contar cuántas veces 
+# aparece una letra en varias palabras
+#  y mostrar solo las 
+# palabras que la contienen.
+
+
+palabras = ["python", "programar", 
+            "divertido", "logica", 
+            "datos"]
+
+print(palabras)
+letra = input("Ingresa una letra para saber cuantas veces aparece: ")
+sum = 0
+
+for palabra in palabras:
+    if letra in palabra:
+        sum += palabra.count(letra)
+if sum == 0:
+    print("No se encontro la letra en la lista de palabras!!")
+else:   
+    print(f"La letra {letra} aparece un total: {sum}")
