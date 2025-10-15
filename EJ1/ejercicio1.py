4a4aef7d09f422a93138e3a6f0904b904218e564
javier020301001 <rjsaransig@uce.edu,ec>
2025-10-15 16:15:58 -0500

    Actualizacion de ejercicio 1

--- EJ1/ejercicio1.py
+++ EJ1/ejercicio1.py
@@ -11,11 +11,15 @@ palabras = ["python", "programar",
 print(palabras)
 letra = input("Ingresa una letra para saber cuantas veces aparece: ")
 sum = 0
+lista_palabras = []
 
 for palabra in palabras:
     if letra in palabra:
         sum += palabra.count(letra)
+        lista_palabras.append(palabra)
+
 if sum == 0:
     print("No se encontro la letra en la lista de palabras!!")
 else:   
     print(f"La letra {letra} aparece un total: {sum}")
+    print(f"Las palabras que contienen la letra {letra} son: {lista_palabras}")
